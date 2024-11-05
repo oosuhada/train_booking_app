d024de13ede75c92ad7bd11295ed65f364b34a82
oosuhada <woosu91@gmail.com>
2024-11-05 20:52:37 +0900

    add passengerCount and isRoundTrip on home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,94 +8,139 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String? departureStation; // 출발역 저장 변수
-  String? arrivalStation; // 도착역 저장 변수
+  String? departureStation;
+  String? arrivalStation;
   bool isRoundTrip = false;
   int passengerCount = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('기차 예매')),
-        body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              // 출발역 및 도착역을 감싸는 박스
-              Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        // 출발역 선택 영역
-                        Expanded(
-                            child: _buildStationSelector(
-                                '출발역', departureStation, () async {
-                          final selectedStation = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => StationListPage(
-                                      selectedStation: arrivalStation)));
-                          if (selectedStation != null) {
-                            setState(() {
-                              departureStation = selectedStation;
-                            });
-                          }
-                        })),
-
-                        // 세로선 고정
-                        Container(
-                            width: 2, height: 50, color: Colors.grey[400]),
-
-                        // 도착역 선택 영역
-                        Expanded(
-                            child: _buildStationSelector('도착역', arrivalStation,
-                                () async {
-                          final selectedStation = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => StationListPage(
-                                      selectedStation: departureStation)));
-                          if (selectedStation != null) {
-                            setState(() {
-                              arrivalStation = selectedStation;
-                            });
-                          }
-                        }))
-                      ])),
-
-              SizedBox(height: 20),
-
-              // 좌석 선택 버튼
-              SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      onPressed: (departureStation != null &&
-                              arrivalStation != null)
-                          ? () {
-                              Navigator.push(
+      appBar: AppBar(title: Text('기차 예매')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('승차권 예매',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('편도'),
+                Switch(
+                  value: isRoundTrip,
+                  onChanged: (value) {
+                    setState(() {
+                      isRoundTrip = value;
+                    });
+                  },
+                ),
+                Text('왕복'),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                      child: _buildStationSelector('출발역', departureStation,
+                          () async {
+                    final selectedStation = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => StationListPage(
+                                selectedStation: arrivalStation)));
+                    if (selectedStation != null) {
+                      setState(() {
+                        departureStation = selectedStation;
+                      });
+                    }
+                  })),
+                  Container(width: 2, height: 50, color: Colors.grey[400]),
+                  Expanded(
+                      child: _buildStationSelector('도착역', arrivalStation,
+                          () async {
+                    final selectedStation = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => StationListPage(
+                                selectedStation: departureStation)));
+                    if (selectedStation != null) {
+                      setState(() {
+                        arrivalStation = selectedStation;
+                      });
+                    }
+                  })),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('인원: '),
+                DropdownButton<int>(
+                  value: passengerCount,
+                  items:
+                      List.generate(10, (index) => index + 1).map((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (int? newValue) {
+                    if (newValue != null) {
+                      setState(() {
+                        passengerCount = newValue;
+                      });
+                    }
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed:
+                        (departureStation != null && arrivalStation != null)
+                            ? () {
+                                Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => SeatPage(
-                                          departureStation!, arrivalStation!)));
-                            }
-                          : null,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.purple,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 15.0),
-                          child: Text('좌석 선택',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold)))))
-            ])));
+                                    builder: (context) => SeatPage(
+                                      departureStation!,
+                                      arrivalStation!,
+                                      passengerCount,
+                                      isRoundTrip,
+                                    ),
+                                  ),
+                                );
+                              }
+                            : null,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                        child: Text('좌석 선택',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold))))),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _buildStationSelector(String label, String? station, Function onTap) {
