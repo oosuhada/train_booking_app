9e34567d4b3c82b83995373ca0c8a4b91eb06a08
oosuhada <woosu91@gmail.com>
2024-11-06 23:06:26 +0900

    add loading function to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart'; // HomePage import
+import 'dart:async';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,8 +22,47 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.grey[900],
         brightness: Brightness.dark,
       ),
-      themeMode: ThemeMode.system, // 시스템 설정에 따라 테마 변경
-      home: HomePage(),
+      themeMode: ThemeMode.system,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/KRAIL_LOGO.jpg',
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+            SizedBox(height: 20),
+            Text('로딩중입니다...'),
+          ],
+        ),
+      ),
     );
   }
 }
