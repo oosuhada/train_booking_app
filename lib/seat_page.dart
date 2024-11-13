e6ddaf76c56eb02497232b105aef94915f48817e
oosuhada <woosu91@gmail.com>
2024-11-13 11:21:35 +0900

    add AnimationController to seat page

--- lib/seat_page.dart
+++ lib/seat_page.dart
@@ -17,217 +17,283 @@ class SeatPage extends StatefulWidget {
   _SeatPageState createState() => _SeatPageState();
 }
 
-class _SeatPageState extends State<SeatPage> {
+class _SeatPageState extends State<SeatPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
   List<List<bool>> seats =
       List.generate(20, (_) => List.generate(4, (_) => false));
   List<String> selectedSeats = [];
   DateTime selectedDate = DateTime.now();
 
   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('좌석 선택')),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        widget.departure,
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Icon(Icons.arrow_circle_right_outlined, size: 30),
-                  Expanded(
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        widget.arrival,
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back_ios),
-                    onPressed: () {
-                      setState(() {
-                        selectedDate = selectedDate.subtract(Duration(days: 1));
-                      });
-                    },
-                  ),
-                  Text(
-                    DateFormat('yyyy년 MM월 dd일').format(selectedDate),
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.arrow_forward_ios),
-                    onPressed: () {
-                      setState(() {
-                        selectedDate = selectedDate.add(Duration(days: 1));
-                      });
-                    },
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text('선택됨'),
-                  SizedBox(width: 20),
-                  Container(
-                    width: 24,
-                    height: 24,
-                    decoration: BoxDecoration(
-                      color: Colors.purple,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  Text('선택 안됨'),
-                  SizedBox(width: 20),
-                  Container(
-                    width: 24,
-                    height: 24,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: ['A', 'B', '', 'C', 'D']
-                    .map((label) => Text(label, style: TextStyle(fontSize: 18)))
-                    .toList(),
-              ),
-              GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: seats[0].length + 1,
-                  childAspectRatio: 1, // 1로 변경하여 정사각형 비율 유지
-                  crossAxisSpacing: 4, // 가로 간격 추가
-                  mainAxisSpacing: 4, // 세로 간격 추가
-                ),
-                itemCount: seats.length * (seats[0].length + 1),
-                itemBuilder: (context, index) {
-                  if (index % (seats[0].length + 1) == 2) {
-                    int row = index ~/ (seats[0].length + 1) + 1;
-                    return Center(
-                      child:
-                          Text(row.toString(), style: TextStyle(fontSize: 18)),
-                    );
-                  }
-                  int row = index ~/ (seats[0].length + 1);
-                  int col = index % (seats[0].length + 1) > 2
-                      ? index % (seats[0].length + 1) - 1
-                      : index % (seats[0].length + 1);
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        if (seats[row][col]) {
-                          seats[row][col] = false;
-                          selectedSeats.remove(
-                              '${row + 1}${String.fromCharCode(65 + col)}');
-                        } else if (selectedSeats.length <
-                            widget.adultCount +
-                                widget.childCount +
-                                widget.seniorCount) {
-                          seats[row][col] = true;
-                          selectedSeats.add(
-                              '${row + 1}${String.fromCharCode(65 + col)}');
-                        }
-                      });
-                    },
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        color:
-                            seats[row][col] ? Colors.purple : Colors.grey[300],
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                  );
-                },
-              ),
-              SizedBox(height: 20),
-              Text(
-                '선택한 좌석: ${selectedSeats.join(", ")}',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                '어른: ${widget.adultCount}, 어린이: ${widget.childCount}, 경로: ${widget.seniorCount}',
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: selectedSeats.length ==
-                        widget.adultCount +
-                            widget.childCount +
-                            widget.seniorCount
-                    ? () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => PaymentPage(
-                              departure: widget.departure,
-                              arrival: widget.arrival,
-                              seatNumbers: selectedSeats,
-                              isRoundTrip: widget.isRoundTrip,
-                              travelDate: selectedDate,
-                              adultCount: widget.adultCount,
-                              childCount: widget.childCount,
-                              seniorCount: widget.seniorCount,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: Text(
+                            widget.departure,
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
                             ),
                           ),
+                        ),
+                      ),
+                      Icon(Icons.arrow_circle_right_outlined, size: 30),
+                      Expanded(
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: Text(
+                            widget.arrival,
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.arrow_back_ios),
+                        onPressed: () {
+                          setState(() {
+                            selectedDate =
+                                selectedDate.subtract(Duration(days: 1));
+                          });
+                        },
+                      ),
+                      Text(
+                        DateFormat('yyyy년 MM월 dd일').format(selectedDate),
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.arrow_forward_ios),
+                        onPressed: () {
+                          setState(() {
+                            selectedDate = selectedDate.add(Duration(days: 1));
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('선택됨'),
+                      SizedBox(width: 20),
+                      Container(
+                        width: 24,
+                        height: 24,
+                        decoration: BoxDecoration(
+                          color: Colors.purple,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Text('선택 안됨'),
+                      SizedBox(width: 20),
+                      Container(
+                        width: 24,
+                        height: 24,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: ['A', 'B', '', 'C', 'D']
+                        .map((label) =>
+                            Text(label, style: TextStyle(fontSize: 18)))
+                        .toList(),
+                  ),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: seats[0].length + 1,
+                      childAspectRatio: 1,
+                      crossAxisSpacing: 4,
+                      mainAxisSpacing: 4,
+                    ),
+                    itemCount: seats.length * (seats[0].length + 1),
+                    itemBuilder: (context, index) {
+                      if (index % (seats[0].length + 1) == 2) {
+                        int row = index ~/ (seats[0].length + 1) + 1;
+                        return Center(
+                          child: Text(row.toString(),
+                              style: TextStyle(fontSize: 18)),
                         );
                       }
-                    : null,
-                child: Text(
-                  '예매 하기',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                      int row = index ~/ (seats[0].length + 1);
+                      int col = index % (seats[0].length + 1) > 2
+                          ? index % (seats[0].length + 1) - 1
+                          : index % (seats[0].length + 1);
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            if (seats[row][col]) {
+                              seats[row][col] = false;
+                              selectedSeats.remove(
+                                  '${row + 1}${String.fromCharCode(65 + col)}');
+                            } else if (selectedSeats.length <
+                                widget.adultCount +
+                                    widget.childCount +
+                                    widget.seniorCount) {
+                              seats[row][col] = true;
+                              selectedSeats.add(
+                                  '${row + 1}${String.fromCharCode(65 + col)}');
+                            }
+                            if (selectedSeats.isNotEmpty) {
+                              _controller.forward();
+                            } else {
+                              _controller.reverse();
+                            }
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: seats[row][col]
+                                ? Colors.purple
+                                : Colors.grey[300],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      );
+                    },
                   ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: SlideTransition(
+              position: _offsetAnimation,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purple,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  minimumSize: Size(double.infinity, 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      '선택한 좌석: ${selectedSeats.join(", ")}',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '어른: ${widget.adultCount}, 어린이: ${widget.childCount}, 경로: ${widget.seniorCount}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: selectedSeats.length ==
+                              widget.adultCount +
+                                  widget.childCount +
+                                  widget.seniorCount
+                          ? () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PaymentPage(
+                                    departure: widget.departure,
+                                    arrival: widget.arrival,
+                                    seatNumbers: selectedSeats,
+                                    isRoundTrip: widget.isRoundTrip,
+                                    travelDate: selectedDate,
+                                    adultCount: widget.adultCount,
+                                    childCount: widget.childCount,
+                                    seniorCount: widget.seniorCount,
+                                  ),
+                                ),
+                              );
+                            }
+                          : null,
+                      child: Text(
+                        '예매 하기',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        minimumSize: Size(double.infinity, 50),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
